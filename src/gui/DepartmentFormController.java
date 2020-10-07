a6186e02ecc13e8dd3f3a4c19d30e1d580bc3d57
Juliana M Peixoto <julianamattos1998@outlook.com>
2020-10-07 16:37:39 -0300

    Passing a Department object to DepartmentForm view

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,44 +9,58 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 public class DepartmentFormController implements Initializable {
 
-	//Atributos -> Elementos no SceneBuilder
+	// Dependencia para Department
+	private Department entity;
+
+	// Atributos -> Elementos no SceneBuilder
 	@FXML
 	private TextField txtId;
-	
+
 	@FXML
 	private TextField txtName;
-	
+
 	@FXML
 	private Label labelErrorName;
-	
+
 	@FXML
 	private Button btSave;
-	
+
 	@FXML
 	private Button btCancel;
-	
-	//Metodos
-	
-	@FXML
-	public void onBtSaveAction() {	//Tratar os eventos dos botões	
+
+	// Metodos
+
+	public void setDepartment(Department entity) { // Instancia do Department
+		this.entity = entity;
 	}
 
 	@FXML
-	public void onBtCancelAction() { //Tratar os eventos dos botões	
+	public void onBtSaveAction() { // Tratar os eventos dos botões
 	}
 
+	@FXML
+	public void onBtCancelAction() { // Tratar os eventos dos botões
+	}
 
 	@Override
-	public void initialize(URL url, ResourceBundle rb) {		
+	public void initialize(URL arg0, ResourceBundle arg1) {
 	}
-	
-	//Restrições
-	 private void initializeNodes() {
-		 Constraints.setTextFieldInteger(txtId);
-		 Constraints.setTextFieldMaxLength(txtName, 12);
-	 }
+
+	private void initializeNodes() { // Restrições Id, Name
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 12);
+	}
+
+	public void updateFormData() { // Responsavel por popular cxs do formulario atraves do Department entity
+		if (entity == null) { //Prog defensiva
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(String.valueOf(entity.getId())); // Usa valueOf para conversão de Integer para String
+		txtName.setText(entity.getName());
+	}
 
 }
--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -50,8 +50,9 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	public void onBtNewAction(ActionEvent event) { //Tratar os eventos dos botões
 		Stage parentStage = Utils.currentStage(event);
+		Department obj = new Department();
 		// Mostrando o caminho para o metodo onBtNewAction
-		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
+		createDialogForm(obj,"/gui/DepartmentForm.fxml", parentStage);
 	}
 
 	public void setDepartmentService(DepartmentService service) {
@@ -83,10 +84,14 @@ public class DepartmentListController implements Initializable {
 	}
 
 	// Janela formulario Department
-	private void createDialogForm(String absoluteName, Stage parentStage) {
+	private void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
+			
+			DepartmentFormController controller = loader.getController();
+			controller.setDepartment(obj);
+			controller.updateFormData();
 
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Enter Department data: ");
