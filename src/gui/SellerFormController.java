9bd092613f8fad4a396518a117ce7c26af89b724
Juliana M Peixoto <julianamattos1998@outlook.com>
2020-10-08 14:17:29 -0300

    SellerForm

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,147 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable {
+
+	// Dependencia para Seller
+
+	private Seller entity;
+
+	private SellerService service;
+
+	// Listas
+
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>(); // Permiti que outros objetos se inscrevam
+																				// na lista e receberem o evento
+
+	// Atributos -> Elementos no SceneBuilder
+
+	@FXML
+	private TextField txtId;
+
+	@FXML
+	private TextField txtName;
+
+	@FXML
+	private Label labelErrorName;
+
+	@FXML
+	private Button btSave;
+
+	@FXML
+	private Button btCancel;
+
+	// Métodos
+
+	public void setSeller(Seller entity) { // Instanciação do Seller
+		this.entity = entity;
+	}
+
+	public void setSellerService(SellerService service) { // Instanciação do SellerService
+		this.service = service;
+	}
+
+	public void subscribeDataChangeListener(DataChangeListener listener) { // Método para adicionar na lista
+		dataChangeListeners.add(listener);
+	}
+
+	@FXML
+	public void onBtSaveAction(ActionEvent event) { // Tratar os eventos dos botões
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity); // Salver no banco de dados
+			notifyDataChangeListener(); // Notificar Listeners
+			Utils.currentStage(event).close();
+		} catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
+		} catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+	private void notifyDataChangeListener() { // Método para notficar os Listeners
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+
+	}
+
+	private Seller getFormData() { // Responsavel por "pegar" os dados do formulario e "guardar" no obj
+		Seller obj = new Seller();
+
+		ValidationException exception = new ValidationException("Validation error");
+
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Field can't be empty");
+		}
+		obj.setName(txtName.getText());
+
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
+
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) { // Tratar os eventos dos botões
+		Utils.currentStage(event).close();
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() { // Restrições Id, Name
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+
+	public void updateFormData() { // Responsavel por popular cxs do formulario atraves do Seller entity
+		if (entity == null) { // Prog defensiva
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(String.valueOf(entity.getId())); // Usa valueOf para conversão de Integer para String
+		txtName.setText(entity.getName());
+	}
+
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if (fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
+	}
+}
