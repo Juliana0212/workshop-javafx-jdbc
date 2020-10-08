ea78bbb94c0aef5d68107a84b5e0b04513c098de
Juliana M Peixoto <julianamattos1998@outlook.com>
2020-10-08 15:19:59 -0300

    Department ComboBox

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -15,16 +15,24 @@ import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.util.Callback;
+import model.entities.Department;
 import model.entities.Seller;
 import model.exceptions.ValidationException;
+import model.services.DepartmentService;
 import model.services.SellerService;
 
 public class SellerFormController implements Initializable {
@@ -34,6 +42,7 @@ public class SellerFormController implements Initializable {
 	private Seller entity;
 
 	private SellerService service;
+	private DepartmentService departmentService;
 
 	// Listas
 
@@ -58,6 +67,9 @@ public class SellerFormController implements Initializable {
 	private TextField txtBaseSalary;
 
 	@FXML
+	private ComboBox<Department> comboBoxDepartment;
+
+	@FXML
 	private Label labelErrorName;
 
 	@FXML
@@ -75,14 +87,20 @@ public class SellerFormController implements Initializable {
 	@FXML
 	private Button btCancel;
 
+	// Lista
+
+	private ObservableList<Department> obsList;
+
 	// Métodos
 
 	public void setSeller(Seller entity) { // Instanciação do Seller
 		this.entity = entity;
 	}
 
-	public void setSellerService(SellerService service) { // Instanciação do SellerService
+	public void setServices(SellerService service, DepartmentService departmentService) { // Instanciação do
+																							// SellerService
 		this.service = service;
+		this.departmentService = departmentService;
 	}
 
 	public void subscribeDataChangeListener(DataChangeListener listener) { // Método para adicionar na lista
@@ -136,6 +154,12 @@ public class SellerFormController implements Initializable {
 		return obj;
 	}
 
+	public void loadAssociateObjects() {
+		List<Department> list = departmentService.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		comboBoxDepartment.setItems(obsList);
+	}
+
 	@FXML
 	public void onBtCancelAction(ActionEvent event) { // Tratar os eventos dos botões
 		Utils.currentStage(event).close();
@@ -152,6 +176,8 @@ public class SellerFormController implements Initializable {
 		Constraints.setTextFieldMaxLength(txtEmail, 60);
 		Constraints.setTextFieldDouble(txtBaseSalary);
 		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
+		
+		initializeComboBoxDepartment();
 	}
 
 	public void updateFormData() { // Responsavel por popular cxs do formulario atraves do Seller entity
@@ -166,6 +192,12 @@ public class SellerFormController implements Initializable {
 		if (entity.getBirthDate() != null) {
 			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
 		}
+		 if (entity.getDepartment() == null) {
+			 comboBoxDepartment.getSelectionModel().selectFirst();
+		 }
+		 else {
+		comboBoxDepartment.setValue(entity.getDepartment());
+		 }
 	}
 
 	private void setErrorMessages(Map<String, String> errors) {
@@ -175,4 +207,17 @@ public class SellerFormController implements Initializable {
 			labelErrorName.setText(errors.get("name"));
 		}
 	}
+
+	private void initializeComboBoxDepartment() {
+		Callback<ListView<Department>, ListCell<Department>> factory = lv -> new ListCell<Department>() {
+			@Override
+			protected void updateItem(Department item, boolean empty) {
+				super.updateItem(item, empty);
+				setText(empty ? "" : item.getName());
+			}
+		};
+		comboBoxDepartment.setCellFactory(factory);
+		comboBoxDepartment.setButtonCell(factory.call(null));
+	}
+
 }
